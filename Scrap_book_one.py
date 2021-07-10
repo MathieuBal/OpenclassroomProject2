b21603f567eab3cfa2796e2d96c76fea48541f34
Mathieu Balayé <mathieubaldev@outlook.fr>
2021-07-10 13:03:42 +0200

    modification apres session mentor

--- Scrap_book_one.py
+++ Scrap_book_one.py
@@ -21,7 +21,7 @@ def getParseUrl(url):
 	return(soup)
 
 # listes des éléments a récuperer
-title = []
+# title = []
 universal_product_coc = []
 price_including_tax = []
 price_excluding_tax = []
@@ -31,10 +31,12 @@ category = []
 review_rating =[]
 image_url = []
 product_url =[]
+
 # liste des "td" 
 Tds = []
+
 # liste produit regroupant toutes les infos produits
-produit =[title, universal_product_coc, price_excluding_tax, price_including_tax, number_available, product_description, category, review_rating, image_url]
+#produit =[title , universal_product_coc, price_excluding_tax, price_including_tax, number_available, product_description, category, review_rating, image_url]
 
 if response.ok:
   with open('produit.csv', 'w') as outfile :
@@ -44,7 +46,7 @@ if response.ok:
 
     soup = getParseUrl(url)
     #title :
-    title.append(soup.find("div", class_ = re.compile("col-sm-6 product_main")).h1.text)
+    title  = soup.find("div", class_ = re.compile("col-sm-6 product_main")).h1.text
     # find all td in table
     Tds.append(soup.findAll("td"))
 
@@ -62,12 +64,18 @@ if response.ok:
     # description
     product_description.append(soup.find("p", attrs={'class': None}).text.strip())
     # category :
-    category.append(soup.find("a", href = re.compile("../category/books/")).text)
+    category = soup.find("a", href = re.compile("../category/books/")).text
     # review rating :
+    image_div2 = soup.find('div', attrs={'class': 'col-sm-6 product_main'})
+    rating = image_div2.select('p.star-rating')
+    for star in rating:
+        print("===============================",star)
+        rating = star.attrs['class'][-1]
+        print("=====>>>>>>>>",star.attrs['class'])
 
     # image url :
     image_url.append(soup.find("img").get("src").replace("../../","http://books.toscrape.com/"))
     # ajout des infos produit au fichier csv
-    write.writerow(produit)
+    # write.writerow({'title': title, })
 
-print("fin des programmes")
+print("fin des programmes", rating)
