e572ad7ed748e509e1c10650a75b7ee28ff14d8b
mathieu balayé <mathieubalde@outlook.frgit config --global user.email mathieubaldev@outlook.fr>
2021-07-28 14:36:59 +0200

    simplification de l'écriture + écriture du csv

--- Scrap_book_one.py
+++ Scrap_book_one.py
@@ -15,22 +15,22 @@ soup = BeautifulSoup(response.text, 'html.parser')
 
 #print(soup.prettify())
 
-def getParseUrl(url):
+def get_Parse_Url(url):
 	response = requests.get(url)
 	soup = BeautifulSoup(response.text, 'html.parser')
 	return(soup)
 
 # listes des éléments a récuperer
 # title = []
-universal_product_coc = []
-price_including_tax = []
-price_excluding_tax = []
-number_available = []
-product_description = []
-category = []
-review_rating =[]
-image_url = []
-product_url =[]
+# universal_product_coc = []
+# price_including_tax = []
+# price_excluding_tax = []
+# number_available = []
+# product_description = []
+# category = []
+# review_rating =[]
+# image_url = []
+# product_url =[]
 
 # liste des "td" 
 Tds = []
@@ -42,9 +42,9 @@ if response.ok:
   with open('produit.csv', 'w') as outfile :
     write = csv.writer(outfile)
     # creer fichier csv - avec entête.
-    outfile.write('title, universal_product_coc, price_excluding_tax, price_including_tax, number_available, product_description, category, review_rating, image_url\n')
+    # outfile.write('title, universal_product_coc, price_excluding_tax, price_including_tax, number_available, product_description, category, review_rating, image_url\n')
 
-    soup = getParseUrl(url)
+    soup = get_Parse_Url(url)
     #title :
     title  = soup.find("div", class_ = re.compile("col-sm-6 product_main")).h1.text
     # find all td in table
@@ -52,15 +52,13 @@ if response.ok:
 
     for td in Tds :
       #Upc :
-      universal_product_coc.append(Tds[0][0].text)
+      universal_product_coc = (Tds[0][0].text)
       # price excluding tax :
-      price_excluding_tax.append(Tds[0][2].text)
+      price_excluding_tax = (Tds[0][2].text)
       # price including tax :
-      price_including_tax.append(Tds[0][3].text)
+      price_including_tax = (Tds[0][3].text)
       # number available :
-      number_available.append(Tds[0][5].text)
-    # number available 2nd option:
-    #number_available.append(soup.find("p", class_=re.compile("instock availability")).text.strip())
+      number_available = (Tds[0][5].text)
     # description
     product_description = soup.find("p", attrs={'class': None}).text.strip()
     # category :
@@ -74,8 +72,9 @@ if response.ok:
         # print("=====>>>>>>>>",star.attrs['class'])
 
     # image url :
-    image_url.append(soup.find("img").get("src").replace("../../","http://books.toscrape.com/"))
+    image_url = (soup.find("img").get("src").replace("../../","http://books.toscrape.com/"))
     # ajout des infos produit au fichier csv
-    write.writerow({'title': title,  })
+    write.writerow({'title', 'UPC', 'price_excluding_tax', 'price_including_tax', 'number_available', 'product_description', 'category', 'review_rating', 'image_url', 'product_url'})
+    write.writerow({title, universal_product_coc, price_excluding_tax, number_available, product_description, category, rating + ' out of five', image_url, url})
 
-print("fin des programmes", rating)
+print("fin des programmes", )
