217eab927bfc1bc9776033ec64f289cfaa16604e
mathieu balayé <mathieubalde@outlook.frgit config --global user.email mathieubaldev@outlook.fr>
2021-08-02 15:04:07 +0200

    tentative de récupération de urls catégories 70% on

--- Scrap_book_two.py
+++ Scrap_book_two.py
@@ -4,10 +4,28 @@ from bs4 import BeautifulSoup
 
 url = "http://books.toscrape.com/index.html"
 
+response = requests.get(url)
+
+#print(response.text)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+
+#print(soup.prettify())
+
 def get_Parse_Url(url):
 	response = requests.get(url)
 	soup = BeautifulSoup(response.text, 'html.parser')
 	return(soup)
 
+# scraper les urls des catégories 
+
+if response.ok:
+	category_urls = []
 	soup = get_Parse_Url(url)
-	print(response.text)
+	ul = soup.findAll('ul', class_="nav nav-list")
+	for li in ul :
+		soup2 = BeautifulSoup(str(ul), 'html.parser')
+		hrefs = soup2.find_all('a', href=True)
+		#for href in hrefs :
+		    #category_urls.append(hrefs['href'])
+	print(hrefs)
