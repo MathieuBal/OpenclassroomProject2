7b9698fa947eb7ab6398e56ca68a235ca02f29e8
mathieu balayé <mathieubalde@outlook.frgit config --global user.email mathieubaldev@outlook.fr>
2021-08-03 19:02:33 +0200

    nouveaux fichiers structure rebuild

---
+++ App.py
@@ -0,0 +1,16 @@
+import book_info_scrap
+
+def main ():
+    print("début d'application")
+
+def get_Parse_Url(url):
+
+def get_book_info():
+
+def write_csv_file():
+
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
---
+++ book_info_scrap.py
@@ -0,0 +1,58 @@
+# -*- coding: utf8 -*-
+import requests
+from bs4 import BeautifulSoup
+import re
+from gazpacho import Soup
+import csv
+
+
+# 1 : request html + html parser
+
+def get_Parse_Url(url):
+  url = 'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
+	response = requests.get(url)
+	soup = BeautifulSoup(response.text, 'html.parser')
+	return(soup)
+
+# 2 : scrap book information 
+
+def get_book_info():
+  # liste des "td" 
+  Tds = []
+  if response.ok:
+    soup = get_Parse_Url(url)
+    #title :
+    title  = soup.find("div", class_ = re.compile("col-sm-6 product_main")).h1.text
+    # find all td in table
+    Tds.append(soup.findAll("td"))
+    for td in Tds :
+      #Upc :
+      universal_product_coc = (Tds[0][0].text)
+      # price excluding tax :
+      price_excluding_tax = (Tds[0][2].text)
+      # price including tax :
+      price_including_tax = (Tds[0][3].text)
+      # number available :
+      number_available = (Tds[0][5].text)
+    # description
+    product_description = soup.find("p", attrs={'class': None}).text.strip()
+    # category :
+    category = soup.find("a", href = re.compile("../category/books/")).text
+    # review rating :
+    image_div2 = soup.find('div', attrs={'class': 'col-sm-6 product_main'})
+    rating = image_div2.select('p.star-rating')
+    for star in rating:
+        rating = star.attrs['class'][-1]
+    # image url :
+    image_url = (soup.find("img").get("src").replace("../../","http://books.toscrape.com/"))
+
+# 3 : Open and write cvs file
+
+def write_csv_file():
+  with open('produit.csv', 'w') as outfile :
+    write = csv.writer(outfile)
+    write.writerow(['title', 'UPC', 'price_excluding_tax', 'price_including_tax', 'number_available', 'product_description', 'category', 'review_rating', 'image_url', 'product_url'])
+    write.writerow([title, universal_product_coc, price_excluding_tax, price_including_tax,  number_available, product_description, category, rating + ' out of five', image_url, url])
+
+
+
