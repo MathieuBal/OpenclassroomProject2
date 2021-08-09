6a2059fbc4fd721fc62dbd5f3f55c3cd24bdd94e
mathieu balayé <mathieubalde@outlook.frgit config --global user.email mathieubaldev@outlook.fr>
2021-08-09 14:49:29 +0200

    update majeur des scripts, correction pylint 6.86/10

--- App.py
+++ App.py
@@ -2,17 +2,17 @@ import book_info_scrap
 
 def main ():
     print("début d'application")
-
-def get_parse_url():
     print("récupération des données html")
-
-def get_book_info():
+    url = 'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
+    book_info_scrap.get_parse_url(url)
     print("récupération htlm : ok")
     print("récupération informations des livres")
-
-def write_csv_file():
+    book_info_scrap.get_book_info()
     print("informations récupérées")
     print("création d'un fichier csv ")
+    book_info_scrap.write_csv_file()
+    print("fichier csv créé avec succès")
+    print("fin d'application")
 
 
 
--- book_info_scrap.py
+++ book_info_scrap.py
@@ -18,37 +18,43 @@ def get_parse_url(url):
 def get_book_info():
     """ liste des "td" """
     tds_list = []
-    if response.ok:
-        soup = get_parse_url(url)
-        # title :
-        title  = soup.find("div", class_ = re.compile("col-sm-6 product_main")).h1.text
-        # find all td in table
-        tds_list.append(soup.findAll("td"))
-        for td in tds_list :
-            # Upc :
-            universal_product_coc = (tds_list[0][0].text)
-            # price excluding tax :
-            price_excluding_tax = (tds_list[0][2].text)
-            # price including tax :
-            price_including_tax = (tds_list[0][3].text)
-            # number available :
-            number_available = (tds_list[0][5].text)
-        # description
-        product_description = soup.find("p", attrs={'class': None}).text.strip()
-        # category :
-        category = soup.find("a", href = re.compile("../category/books/")).text
-        # review rating :
-        image_div2 = soup.find('div', attrs={'class': 'col-sm-6 product_main'})
-        rating = image_div2.select('p.star-rating')
-        for star in rating:
-            rating = star.attrs['class'][-1]
-        # image url :
-        image_url = (soup.find("img").get("src").replace("../../","http://books.toscrape.com/"))
+    url = 'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
+    soup = get_parse_url(url)
+    # title :
+    title  = soup.find("div", class_ = re.compile("col-sm-6 product_main")).h1.text
+    # find all td in table
+    tds_list.append(soup.findAll("td"))
+    for td in tds_list :
+        # Upc :
+        universal_product_coc = (tds_list[0][0].text)
+        # price excluding tax :
+        price_excluding_tax = (tds_list[0][2].text)
+        # price including tax :
+        price_including_tax = (tds_list[0][3].text)
+        # number available :
+        number_available = (tds_list[0][5].text)
+    # description
+    product_description = soup.find("p", attrs={'class': None}).text.strip()
+    # category :
+    category = soup.find("a", href = re.compile("../category/books/")).text
+    # review rating :
+    image_div2 = soup.find('div', attrs={'class': 'col-sm-6 product_main'})
+    rating = image_div2.select('p.star-rating')
+    for star in rating:
+        rating = star.attrs['class'][-1]
+    # image url :
+    image_url = (soup.find("img").get("src").replace("../../","http://books.toscrape.com/"))
+    book_data = [title, universal_product_coc, price_excluding_tax,
+    price_including_tax,  number_available, product_description,
+    category, rating + ' out of five', image_url, url]
 
 """3 : Open and write cvs file"""
 def write_csv_file():
     """ creat produit.csv file / write header and book information """
+    headername = ['title', 'UPC', 'price_excluding_tax', 'price_including_tax',
+    'number_available', 'product_description', 'category', 'review_rating',
+    'image_url', 'product_url']
     with open('produit.csv', 'w') as outfile :
         write = csv.writer(outfile)
-        write.writerow(['title', 'UPC', 'price_excluding_tax', 'price_including_tax', 'number_available', 'product_description', 'category', 'review_rating', 'image_url', 'product_url'])
-        write.writerow([title, universal_product_coc, price_excluding_tax, price_including_tax,  number_available, product_description, category, rating + ' out of five', image_url, url])
+        write.writerow([headername,])
+        write.writerow([book_data,])
